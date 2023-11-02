24ea82b29769569c492c04d059c617529190f2fa
ahsanwebengr <ahsanwebengr@gmail.com>
2023-11-02 22:25:18 +0500

    feat: Form validation in Contact us

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -4,13 +4,20 @@ import NewsLetter from '../components/NewsLetter';
 import { BiTime } from 'react-icons/bi';
 import { LuPhoneCall } from 'react-icons/lu';
 import { FaMapMarkerAlt } from 'react-icons/fa';
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
     return (
         <>
             <SectionBanner heading={`#Let'sTalk`} subHeading={'LEAVE A MESSAGE. We love from here you!'} bgImg={'cart-banner'} />
             <div className="container py-10">
-                <div class="mb-4">
+                <div className="mb-4">
                     <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
                         <p className="text-base font-semibold uppercase tracking-wide text-purple-600">Contact</p>
                         <h2 className="font-heading mb-4 font-bold tracking-tight text-gray-900 text-3xl sm:text-5xl">Get in Touch</h2>
@@ -18,10 +25,8 @@ const Contact = () => {
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2">
                     <div className="h-full pr-6">
-                        <p className="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
-                            Class aptent taciti sociosqu ad
-                            litora torquent per conubia nostra, per inceptos himenaeos. Duis nec ipsum orci. Ut scelerisque
-                            sagittis ante, ac tincidunt sem venenatis ut.
+                        <p className="mt-3 mb-12 text-lg text-gray-600 ">
+                            Let us be your strategic partner in building a successful online store. Don't miss out on the opportunity to transform your business. Contact us today and take the first step towards e-commerce success.
                         </p>
                         <ul className="mb-6 md:mb-0">
                             <li className="flex">
@@ -31,19 +36,19 @@ const Contact = () => {
                                 <div className="ml-4 mb-4">
                                     <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Our Address
                                     </h3>
-                                    <p className="text-gray-600 dark:text-slate-400">1230 Maecenas Street Donec Road</p>
-                                    <p className="text-gray-600 dark:text-slate-400">New York, EEUU</p>
+                                    <p className="text-gray-600 ">1230 Maecenas Street Donec Road</p>
+                                    <p className="text-gray-600 ">New York, EEUU</p>
                                 </div>
                             </li>
                             <li className="flex">
-                                <div className="flex h-10 w-10 items-center justify-center rounded bg-purple-900 ">
+                                <div className="flex h-10 w-10 items-center justify-center rounded bg-purple-900">
                                     <LuPhoneCall className='text-white' />
                                 </div>
                                 <div className="ml-4 mb-4">
                                     <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Contact
                                     </h3>
-                                    <p className="text-gray-600 dark:text-slate-400">Mobile: +1 (123) 456-7890</p>
-                                    <p className="text-gray-600 dark:text-slate-400">Mail: tailnext@gmail.com</p>
+                                    <p className="text-gray-600 ">Mobile: +1 (123) 456-7890</p>
+                                    <p className="text-gray-600 ">Mail: tailnext@gmail.com</p>
                                 </div>
                             </li>
                             <li className="flex">
@@ -53,33 +58,60 @@ const Contact = () => {
                                 <div className="ml-4 mb-4">
                                     <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">Working
                                         hours</h3>
-                                    <p className="text-gray-600 dark:text-slate-400">Monday - Friday: 08:00 - 17:00</p>
-                                    <p className="text-gray-600 dark:text-slate-400">Saturday &amp; Sunday: 08:00 - 12:00</p>
+                                    <p className="text-gray-600 ">Monday - Friday: 08:00 - 17:00</p>
+                                    <p className="text-gray-600 ">Saturday &amp; Sunday: 08:00 - 12:00</p>
                                 </div>
                             </li>
                         </ul>
                     </div>
-                    <div class="h-fit max-w-6xl p-5 md:p-12" id="form">
-                        <h2 class="mb-4 text-2xl font-bold">LEAVE A MESSAGE</h2>
-                        <form id="contactForm">
-                            <div class="mb-6">
-                                <div class="mx-0 mb-1 sm:mb-4">
-                                    <div class="mx-0 mb-1 sm:mb-4">
-                                        <label for="name" class="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="name" autocomplete="given-name" placeholder="Enter Name" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0" name="name" />
+                    <div className="shadow-lg p-5 md:p-12" id="form">
+                        <h2 className="mb-4 text-2xl font-bold">LEAVE A MESSAGE</h2>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="mb-6">
+                                <div className="mx-0 mb-1 sm:mb-4">
+
+                                    <div className="mx-0 mb-1 sm:mb-4">
+                                        <label htmlFor="name" className="pb-1 text-xs uppercase tracking-wider"></label>
+                                        <input type="text" placeholder="Enter Name*"
+                                            {...register('name', { required: 'Name required', })}
+                                            className={`mb-2 w-full rounded-md border ${errors.name ? 'border-red-500' : 'border-gray-400'}  py-2 pl-2 pr-4 shadow-md sm:mb-0 focus:border-purple-400 focus:outline-none focus:border-2`}
+                                        />
+                                        {errors.name?.type === "required" && (
+                                            <small className="text-red-500">{errors.name.message}</small>
+                                        )}
                                     </div>
-                                    <div class="mx-0 mb-1 sm:mb-4">
-                                        <label for="email" class="pb-1 text-xs uppercase tracking-wider"></label><input type="email" id="email" autocomplete="email" placeholder="Enter Email Address" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0" name="email" />
+
+                                    <div className="mx-0 mb-1 sm:mb-4">
+                                        <label htmlFor="email" className="pb-1 text-xs uppercase tracking-wider"></label>
+                                        <input type="email" placeholder="Enter Email Address*"
+                                            {...register('email', {
+                                                required: 'Email required', pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                    message: 'Invalid email address',
+                                                },
+                                            })}
+                                            className={`mb-2 w-full rounded-md border ${errors.email ? 'border-red-500' : 'border-gray-400'}  py-2 pl-2 pr-4 shadow-md sm:mb-0 focus:border-purple-400 focus:outline-none focus:border-2`}
+                                        />
+                                        {errors.email?.type === "required" && (
+                                            <small className='text-red-500'>{errors.email.message}</small>
+                                        )}
                                     </div>
-                                    <div class="mx-0 mb-1 sm:mb-4">
-                                        <label for="subject" class="pb-1 text-xs uppercase tracking-wider"></label><input type="text" id="subject" autocomplete="email" placeholder="Enter Subject" class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0" name="email" />
+                                    <div className="mx-0 mb-1 sm:mb-4">
+                                        <label htmlFor="subject" className="pb-1 text-xs uppercase tracking-wider"></label>
+                                        <input type="text" placeholder="Enter Subject*"
+                                            {...register('subject', { required: 'Subject required', })} className={`mb-2 w-full rounded-md border ${errors.subject ? 'border-red-500' : 'border-gray-400'}  py-2 pl-2 pr-4 shadow-md sm:mb-0 focus:border-purple-400 focus:outline-none focus:border-2`} />
+                                        {errors.subject?.type === "required" && (
+                                            <small className='text-red-500'>{errors.subject.message}</small>
+                                        )}
                                     </div>
                                 </div>
-                                <div class="mx-0 mb-1 sm:mb-4">
-                                    <label for="textarea" class="pb-1 text-xs uppercase tracking-wider"></label><textarea id="textarea" name="textarea" cols="30" rows="5" placeholder="Write your message..." class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0"></textarea>
+                                <div className="mx-0 mb-1 sm:mb-4">
+                                    <label htmlFor="textarea" className="pb-1 text-xs uppercase tracking-wider"></label>
+                                    <textarea cols="30" rows="5" placeholder="Write your message..." className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md sm:mb-0 focus:border-purple-400 focus:outline-none focus:border-2"></textarea>
                                 </div>
                             </div>
-                            <div class="text-center">
-                                <button type="submit" class="w-full bg-purple-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
+                            <div className="text-center">
+                                <button type="submit" className="w-full bg-purple-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">Send Message</button>
                             </div>
                         </form>
                     </div>
