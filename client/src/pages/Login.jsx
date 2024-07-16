89f32af5e0f440ea7fa98807610fbe3935e0120b
ahsanwebengr <ahsanwebengr@gmail.com>
2024-07-16 19:59:27 +0500

    fix: loading states in the auth screens

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { userAvatar } from '../assets';
 import { shopping } from '../assets';
-import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { addUser } from '../redux/userSlice';
@@ -9,140 +14,167 @@ import { AiOutlineGoogle } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
-    const googleLogin = (e) => {
-        e.preventDefault();
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                const user = result.user;
-                dispatch(
-                    addUser({
-                        _id: user.uid,
-                        name: user.displayName,
-                        image: user.photoURL,
-                        email: user.email
-                    })
-                );
+  const googleLogin = e => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        dispatch(
+          addUser({
+            _id: user.uid,
+            name: user.displayName,
+            image: user.photoURL,
+            email: user.email,
+          })
+        );
 
-                setTimeout(() => {
-                    navigate('/');
-                }, 500);
-            }).catch((error) => {
-                console.log(error);
-            });
-    };
+        setTimeout(() => {
+          navigate('/');
+        }, 500);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    const handleSignIn = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        try {
-            const userCredential = await signInWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
+  const handleSignIn = async e => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
 
-            dispatch(
-                addUser({
-                    _id: user?.uid,
-                    name: user?.displayName,
-                    image: user?.photoURL ? user?.photoURL : userAvatar,
-                    email: user?.email
-                })
-            );
+      dispatch(
+        addUser({
+          _id: user?.uid,
+          name: user?.displayName,
+          image: user?.photoURL ? user?.photoURL : userAvatar,
+          email: user?.email,
+        })
+      );
 
-            toast.success('Login successfully.');
-            navigate('/');
-            setLoading(false);
-        } catch (error) {
-            const errorMessage = error.message;
-            if (errorMessage === 'Firebase: Error (auth/invalid-login-credentials).') {
-                toast.error('Invalid login credentials.');
-            } else {
-                toast.error(errorMessage);
-            }
-        }
-    };
-    return (
-        <>
-            <div className="w-full h-screen flex justify-center items-center py-10">
-                <div className="lg:grid lg:grid-cols-2 w-full">
-                    {/* Left Grid (Sign Up Form) */}
-                    <div className="rounded-lg lg:rounded-r-none border bg-white lg:flex lg:justify-center lg:items-center lg:h-screen">
-                        <div className="lg:w-[25rem] p-4 sm:w-full py-4">
-                            <h2 className="text-2xl font-semibold mb-4">Sign in to your Account</h2>
-                            <form id='sign-in-form' onSubmit={handleSignIn}>
-                                <div className="mb-4">
-                                    <label htmlFor="email" className="block text-sm font-medium text-gray-600">Email Address</label>
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        autoComplete="autoComplete"
-                                        className="input input-bordered input-primary w-full"
-                                        required
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        value={email}
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label htmlFor="password" className="block text-sm font-medium text-gray-600">Password</label>
-                                    <input
-                                        id='password'
-                                        type="password"
-                                        name="password"
-                                        className="input input-bordered input-primary w-full"
-                                        required
-                                        onChange={(e) => setPassword(e.target.value)}
-                                        value={password}
-                                    />
-                                </div>
-                                <div className="mb-4 flex gap-1 items-center">
-                                    <input
-                                        type="checkbox"
-                                        name="remember"
-                                        id='remember'
-                                        className='focus:ring-purple-400 focus:ring-1 rounded-md w-4 h-4'
-                                    />
-                                    <label htmlFor="remember" className="block text-sm font-medium text-gray-600">Remember me</label>
-                                </div>
-                                <button
-                                    type="submit"
-                                    className="btn btn-primary text-white btn-block text-lg"
-                                >
-                                    {loading ? <span className="loading loading-spinner loading-sm"></span> : 'Login'}
-                                </button>
-                            </form>
-
-                            <p className='mt-3 text-gray-500 text-base'>Don't have an account <Link to={'/signup'} className='link link-primary'>Create</Link></p>
-
-                            <div className="divider">OR</div>
-
-                            <div className="my-8 w-full">
-                                <button onClick={googleLogin} className="btn btn-neutral text-white btn-block text-lg">
-                                    <AiOutlineGoogle />
-                                    <span> Google</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* Right Grid (Image) */}
-                    <div className="hidden  lg:block lg:w-full bg-white lg:h-screen" >
-                        <img
-                            src={shopping}
-                            alt="E-commerce"
-                            className='w-full h-full object-cover'
-                        />
-                    </div>
+      toast.success('Login successfully.');
+      navigate('/');
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      const errorMessage = error.message;
+      if (errorMessage === 'Firebase: Error (auth/invalid-login-credentials).') {
+        toast.error('Invalid login credentials.');
+      } else {
+        toast.error(errorMessage);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <div className='w-full h-screen flex justify-center items-center py-10'>
+        <div className='lg:grid lg:grid-cols-2 w-full'>
+          {/* Left Grid (Sign Up Form) */}
+          <div className='rounded-lg lg:rounded-r-none border bg-white lg:flex lg:justify-center lg:items-center lg:h-screen'>
+            <div className='lg:w-[25rem] p-4 sm:w-full py-4'>
+              <h2 className='text-2xl font-semibold mb-4'>Sign in to your Account</h2>
+              <form id='sign-in-form' onSubmit={handleSignIn}>
+                <div className='mb-4'>
+                  <label
+                    htmlFor='email'
+                    className='block text-sm font-medium text-gray-600'
+                  >
+                    Email Address
+                  </label>
+                  <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    autoComplete='autoComplete'
+                    className='input input-bordered input-primary w-full'
+                    required
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                  />
                 </div>
+                <div className='mb-4'>
+                  <label
+                    htmlFor='password'
+                    className='block text-sm font-medium text-gray-600'
+                  >
+                    Password
+                  </label>
+                  <input
+                    id='password'
+                    type='password'
+                    name='password'
+                    className='input input-bordered input-primary w-full'
+                    required
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                  />
+                </div>
+                <div className='mb-4 flex gap-1 items-center'>
+                  <input
+                    type='checkbox'
+                    name='remember'
+                    id='remember'
+                    className='focus:ring-purple-400 focus:ring-1 rounded-md w-4 h-4'
+                  />
+                  <label
+                    htmlFor='remember'
+                    className='block text-sm font-medium text-gray-600'
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <button
+                  type='submit'
+                  className='btn btn-primary text-white btn-block text-lg'
+                >
+                  {loading ? (
+                    <span className='loading loading-spinner loading-sm'></span>
+                  ) : (
+                    'Login'
+                  )}
+                </button>
+              </form>
+
+              <p className='mt-3 text-gray-500 text-base'>
+                Do not have an account{' '}
+                <Link to={'/signup'} className='link link-primary'>
+                  Create
+                </Link>
+              </p>
+
+              <div className='divider'>OR</div>
+
+              <div className='my-8 w-full'>
+                <button
+                  onClick={googleLogin}
+                  className='btn btn-neutral text-white btn-block text-lg'
+                >
+                  <AiOutlineGoogle />
+                  <span> Google</span>
+                </button>
+              </div>
             </div>
-        </>
-    );
+          </div>
+
+          {/* Right Grid (Image) */}
+          <div className='hidden  lg:block lg:w-full bg-white lg:h-screen'>
+            <img src={shopping} alt='E-commerce' className='w-full h-full object-cover' />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,169 +1,187 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { shopping } from '../assets';
 import { Link, useNavigate } from 'react-router-dom';
 import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth';
 import { toast } from 'react-toastify';
 
 const SignUp = () => {
-    const navigate = useNavigate();
-    const auth = getAuth();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [imageFile, setImageFile] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const [setImageFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const handleImageChange = e => {
+    const file = e.target.files[0];
 
-    const handleImageChange = (e) => {
-        const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const blob = new Blob([file], { type: file.type });
+        const objectURL = URL.createObjectURL(blob);
+        setImage(objectURL);
+        setImageFile(blob);
+      };
+      reader.readAsArrayBuffer(file);
+    }
+  };
 
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = () => {
-                const blob = new Blob([file], { type: file.type });
-                const objectURL = URL.createObjectURL(blob);
-                setImage(objectURL);
-                setImageFile(blob);
-            };
-            reader.readAsArrayBuffer(file);
+  const handleSignUp = e => {
+    e.preventDefault();
+    setLoading(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredential => {
+        const user = userCredential.user;
+        updateProfile(user, {
+          displayName: name,
+          photoURL: image,
+        })
+          .then(() => {
+            toast.success(`${name} your account created`);
+          })
+          .catch(error => {
+            console.error(error.message);
+          });
+
+        setEmail('');
+        setPassword('');
+        setImage('');
+        setName('');
+
+        setLoading(false);
+        navigate('/login');
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        if (errorCode === 'auth/email-already-in-use') {
+          toast.error('Email is already in use. Please use different email.');
+        } else {
+          console.error(errorMessage);
+          toast.error(errorMessage);
         }
-    };
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
+  return (
+    <div className='w-full h-screen flex justify-center items-center py-10'>
+      <div className='lg:grid lg:grid-cols-2 w-full'>
+        {/* Left Grid (Sign Up Form) */}
+        <div className='rounded-lg lg:rounded-r-none border bg-white lg:flex lg:justify-center lg:items-center lg:h-screen'>
+          <div className='lg:w-[25rem] p-4 sm:w-full sm:h-[70vh] md:h-[60vh]'>
+            <h2 className='text-3xl font-semibold mb-4'>Sign Up Here</h2>
+            <form id='sign-up-form' onSubmit={handleSignUp}>
+              <div className='mb-4'>
+                <label
+                  htmlFor='username'
+                  className='block text-sm font-medium text-gray-600'
+                >
+                  Username
+                  <span className='text-red-500'>*</span>
+                </label>
+                <input
+                  type='text'
+                  id='username'
+                  autoComplete='autocomplete'
+                  name='username'
+                  className='input input-bordered input-primary w-full'
+                  required
+                  onChange={e => setName(e.target.value)}
+                  value={name}
+                />
+              </div>
+              <div className='mb-4'>
+                <label
+                  htmlFor='image'
+                  className='block text-sm font-medium text-gray-600'
+                >
+                  Image
+                </label>
+                <input
+                  type='file'
+                  id='image'
+                  className='file-input file-input-primary w-full'
+                  accept='image/*'
+                  onChange={handleImageChange}
+                />
+              </div>
+              {image && (
+                <img src={image} alt='Selected Image' style={{ maxWidth: '100px' }} />
+              )}
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
-        setLoading(true);
+              <div className='mb-4'>
+                <label
+                  htmlFor='email'
+                  className='block text-sm font-medium text-gray-600'
+                >
+                  Email
+                  <span className='text-red-500'>*</span>
+                </label>
+                <input
+                  type='email'
+                  id='email'
+                  autoComplete='autocomplete'
+                  name='email'
+                  className='input input-bordered input-primary w-full'
+                  required
+                  onChange={e => setEmail(e.target.value)}
+                  value={email}
+                />
+              </div>
+              <div className='mb-4'>
+                <label
+                  htmlFor='password'
+                  className='block text-sm font-medium text-gray-600'
+                >
+                  Password
+                  <span className='text-red-500'>*</span>
+                </label>
+                <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  className='input input-bordered input-primary w-full'
+                  required
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                />
+              </div>
+              <p className='my-4 text-gray-500 text-base'>
+                Already have an account{' '}
+                <Link to={'/login'} className='link link-primary'>
+                  Login
+                </Link>
+              </p>
 
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user;
-                updateProfile(user, {
-                    displayName: name,
-                    photoURL: image,
-                }).then(() => {
-                    toast.success(`${name} your account created`);
-                }).catch((error) => {
-                    console.error(error.message);
-                });
-
-                setEmail('');
-                setPassword('');
-                setImage('');
-                setName('');
-
-                setLoading(false);
-                navigate('/login');
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-
-                if (errorCode === 'auth/email-already-in-use') {
-                    toast.error('Email is already in use. Please use different email.');
-                } else {
-                    console.error(errorMessage);
-                    toast.error(errorMessage);
-                }
-            });
-    };
-
-
-    return (
-        <div className="w-full h-screen flex justify-center items-center py-10">
-            <div className="lg:grid lg:grid-cols-2 w-full">
-                {/* Left Grid (Sign Up Form) */}
-                <div className="rounded-lg lg:rounded-r-none border bg-white lg:flex lg:justify-center lg:items-center lg:h-screen">
-                    <div className="lg:w-[25rem] p-4 sm:w-full sm:h-[70vh] md:h-[60vh]">
-                        <h2 className="text-3xl font-semibold mb-4">Sign Up Here</h2>
-                        <form id='sign-up-form' onSubmit={handleSignUp}>
-                            <div className="mb-4">
-                                <label htmlFor="username" className="block text-sm font-medium text-gray-600">
-                                    Username
-                                    <span className='text-red-500'>*</span>
-                                </label>
-                                <input
-                                    type="text"
-                                    id="username"
-                                    autoComplete="autocomplete"
-                                    name="username"
-                                    className="input input-bordered input-primary w-full"
-                                    required
-                                    onChange={(e) => setName(e.target.value)}
-                                    value={name}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="image" className="block text-sm font-medium text-gray-600">Image</label>
-                                <input
-                                    type="file"
-                                    id='image'
-                                    className="file-input file-input-primary w-full"
-                                    accept="image/*"
-                                    onChange={handleImageChange} />
-                            </div>
-                            {image && (
-                                <img
-                                    src={image}
-                                    alt="Selected Image"
-                                    style={{ maxWidth: '100px' }}
-                                />
-                            )}
-
-                            <div className="mb-4">
-                                <label htmlFor="email" className="block text-sm font-medium text-gray-600">
-                                    Email
-                                    <span className='text-red-500'>*</span>
-                                </label>
-                                <input
-                                    type="email"
-                                    id="email"
-                                    autoComplete="autocomplete"
-                                    name="email"
-                                    className="input input-bordered input-primary w-full"
-                                    required
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    value={email}
-                                />
-                            </div>
-                            <div className="mb-4">
-                                <label htmlFor="password" className="block text-sm font-medium text-gray-600">
-                                    Password
-                                    <span className='text-red-500'>*</span>
-                                </label>
-                                <input
-                                    type="password"
-                                    id='password'
-                                    name="password"
-                                    className="input input-bordered input-primary w-full"
-                                    required
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    value={password}
-                                />
-                            </div>
-                            <p className='my-4 text-gray-500 text-base'>Already have an account <Link to={'/login'} className='link link-primary'>Login</Link></p>
-
-                            <button
-                                type="submit"
-                                className="btn bg-purple-700 hover:bg-purple-500 text-white btn-block text-lg"
-                            >
-                                {loading ? <span className="loading loading-spinner loading-sm"></span> : 'Signup'}
-                            </button>
-                        </form>
-                    </div>
-                </div>
-
-                {/* Right Grid (Image) */}
-                <div className="hidden  lg:block lg:w-full bg-white lg:h-screen" >
-                    <img
-                        src={shopping}
-                        alt="E-commerce"
-                        className='w-full h-full object-cover'
-                    />
-                </div>
-            </div>
+              <button
+                type='submit'
+                className='btn bg-purple-700 hover:bg-purple-500 text-white btn-block text-lg'
+              >
+                {loading ? (
+                  <span className='loading loading-spinner loading-sm'></span>
+                ) : (
+                  'Signup'
+                )}
+              </button>
+            </form>
+          </div>
         </div>
-    );
+
+        {/* Right Grid (Image) */}
+        <div className='hidden  lg:block lg:w-full bg-white lg:h-screen'>
+          <img src={shopping} alt='E-commerce' className='w-full h-full object-cover' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
